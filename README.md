17c3a2734b01596e4bccfc16174ff8854a626403
git-uname <a.french@mail.utoronto.ca>
2017-04-16 23:31:09 -0400

    Optimizations, new features, and readability fixes
     - Reduce memory usage when reading large files
     - Fix infinite recursion for configs that execute each other
     - Add --cleanlines option to remove excess newlines
     - Move old --cleanlines functionality to --rmlines
     - Add --rmcomments option to remove all comments
     - Add --outputdir option to specify alternate output directory

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
-# Source Config Cleaner
+## Source Config Cleaner ##
 Cleans up config files that are used by Source Engine games such as TF2 and CSGO 
 
-####Usage:
-	cleaner.py [-h] [-v] [-cl] file [file ...]
+#### Usage ####
+```
+cleaner.py [-h] [-o] [-v] [-rl] [-cl] [-R] [-re] [-rc] file [file ...]
 
-	optional arguments:
-  		-h, --help         show this help message and exit
-  		-v, --verbose      Log operations verbosely
-  		-cl, --cleanlines  Remove empty lines from output file
-  		-r, --recursive    Clean other config files executed in the config file recursively
+positional arguments:
+  file               File to parse
+
+optional arguments:
+  -h, --help         Show help message and exit
+  -o, --outputdir	 Where to store output files. Default ./output/
+  -v, --verbose      Log operations verbosely
+  -rl, --rmlines     Remove empty lines from output file
+  -cl, --cleanlines  Replace multiple empty lines with one empty line
+  -R, --recursive    Run cleaner recursively on executed config files
+  -re, --reverse     When overwriting binds and aliases use the first
+                     assignment in the file rather than the last
+  -rc, --rmcomments  Remove lines that are only comments. Leaves inline
+                     comments intact.
+```
 	
-####Built with:
-	- Python 2.7.6
+#### Built with ####
+	- Python 2.7
--- cleaner.py
+++ cleaner.py
@@ -1,109 +1,154 @@
 import argparse
-import os, sys
+import os
+import sys
 
 
 def Main():
-    parser = argparse.ArgumentParser(description = "Clean up properly formatted Source cfg files")
-    parser.add_argument('file', type=argparse.FileType('r'), nargs='+')
-    parser.add_argument("-v", "--verbose", help = "Log operations verbosely", action ="store_true")
-    parser.add_argument("-cl", "--cleanlines", help = "Remove empty lines from output file", action = "store_true")
-    parser.add_argument("-r", "--recursive", help = "Run cleaner recursively on executed config files", action="store_true")
-    args = parser.parse_args()
-    
-    global verbose, rm_empty_lines, recursive
+    parser = argparse.ArgumentParser(
+        description="Refactor Source config files")
+    parser.add_argument("file", help="File to parse", nargs="+")
+        parser.add_argument(
+        "-o", "--outputdir", help="Where to store output files. Default ./output", action="store")
+    parser.add_argument("-v", "--verbose",
+                        help="Log operations verbosely", action="store_true")
+    parser.add_argument(
+        "-rl", "--rmlines", help="Remove empty lines from output file", action="store_true")
+    parser.add_argument("-cl", "--cleanlines",
+                        help="Replace multiple empty lines with one empty line", action="store_true")
+    parser.add_argument(
+        "-R", "--recursive", help="Run cleaner recursively on executed config files", action="store_true")
+    parser.add_argument(
+        "-re", "--reverse", help="When overwriting binds and aliases use the first assignment in the file rather than the last", action="store_true")
+    parser.add_argument("-rc", "--rmcomments",
+                        help="Remove comment lines. Leaves inline comments intact.", action="store_true")
 
+    args = parser.parse_args()
+
+    global verbose, rm_empty_lines, reduce_empty_lines, recursive, reverse, rm_comments, output_file_loc
+
+    output_file_loc = args.outputdir if args.outputdir is not None else "./output/"
     verbose = args.verbose
-    rm_empty_lines = args.cleanlines
+    rm_empty_lines = args.rmlines
     recursive = args.recursive
-    
+    reduce_empty_lines = args.cleanlines
+    reverse = args.reverse
+    rm_comments = args.rmcomments
+
     if not os.path.exists("output"):
         os.makedirs("output")
-    
-    for f in args.file:
-        if verbose:
-            print "Parsing file " + f.name + "\n"
-        parseFile(f)
-        f.close()
-        if verbose:
-            print "Done parsing file " + f.name + "\n"
 
-# Make sure ./output exists before running this
-def parseFile(infile):
-    outfile = open('output/' + infile.name, 'w+')
+    for filename in args.file:
+        if verbose:
+            print "Parsing file " + filename + "\n"
+        parse_status = parseFile(filename)
+        if parse_status == -1:
+            print "Could not parse file " + filename + ".\n"
+        elif verbose:
+            print "Successfully parsed " + parse_status + " lines of file " + filename + "\n"
+
+# Open file named infile_name and parse it, writing output to output_file_loc/infile_name
+# Returns how many lines were parsed or -1 if an error ocurred
+def parseFile(infile_name, loaded_files={}):
+    outfile = open(output_file_loc + infile_name, 'w+')
     aliases = {}
-    loaded_files = {}
     binds = {}
     output = []
-    lineNo = -1
-    for line in infile:
-        lineNo += 1
-        line = line.rstrip()
-        if line == "":
-            if rm_empty_lines:
-                lineNo -= 1
+    lineNo = 0
+    removedLines = 0
+    with open(infile_name) as infile:
+        for line in infile:
+            lineNo += 1
+            line = line.rstrip()
+            if line == "":
+                if rm_empty_lines:
+                    lineNo -= 1
+                elif reduce_empty_lines:
+                    removedLines += 1
+                else:
+                    output.append(line)
+                continue
             else:
+                removedLines = 0
+            try:
+                split_line = line.split(' ')
+                command = split_line[0].lower()
+                arg = split_line[1]
+                if verbose:
+                    print "Parsing line: " + line
+                if command[:2] == "//":
+                    if rm_comments:
+                        print "Removed comment on line " + lineNo
+                        continue
+                elif command == "alias":
+                    if arg in aliases.keys():
+                        if reverse:
+                            print "Alias " + arg + " ignored on line " + lineNo
+                        elif verbose:
+                            output[aliases[arg]] = line
+                            print "Alias " + arg + " already existed, overwriting"
+                    else:
+                        aliases[arg] = lineNo
+                        output.append(line)
+                        if verbose:
+                            print "Alias " + arg + " parsed"
+                elif command == "bind":
+                    if reverse:
+                        print "Bind " + arg + " ignored on line " + lineNo
+                    else:
+                        if arg.replace('"', "") in binds.keys():
+                            if verbose:
+                                print "Bind " + arg + " already existed at line" + output[binds[arg]] + ", overwriting"
+                            output[binds[arg]] = line
+                        else:
+                            binds[arg] = lineNo
+                            output.append(line)
+                            if verbose:
+                                print "Bind " + arg + " parsed"
+                elif command == "exec":
+                    if arg in loaded_files.keys():
+                        if reverse:
+                            if arg in loaded_files.keys():
+                                print "exec" + arg + " on line " + lineNo + " ignored because " + arg + " was already executed"
+                                continue
+                        else:
+                            output.pop(loaded_files[arg])
+                            if verbose:
+                                print "File " + arg + " already cleaned"
+                    else:
+                        output.append(line)
+                        loaded_files[arg] = lineNo
+                        if verbose:
+                            print "Cleaned file " + arg + (".cfg" if ".cfg" not in arg else "")
+                        if recursive:
+                            if arg in loaded_files:
+                                print arg + " already cleaned, ignoring line " + lineNo
+                            try:
+                                print "Recursively cleaning " + arg + ".cfg"
+                                parseFile(rec_file)
+                                rec_file.close()
+                            except IOError as e:
+                                print "Couldn't open " + arg + ".cfg: {0}".format(e.strerror)
+
+                else:
+                    output.append(line)
+            except IndexError:
                 output.append(line)
-            continue   
-        try:
-            split_line = line.split(' ')
-            command = split_line[0].lower()
-            arg = split_line[1]
-            if verbose:
-                print "Parsing line --- " + line
-            if command == "alias":
-                if arg in aliases.keys():
-                    output[aliases[arg]] = line
-                    if verbose:
-                        print "Alias " + arg + " already existed, overwriting"  
-                else:
-                    aliases[arg] = lineNo
-                    output.append(line)
-                    if verbose:
-                        print "New alias " + arg + " added"                
-            elif command == "bind":
-                if arg.replace('"', "") in binds.keys():
-                    output[binds[arg]] = line                    
-                    if verbose:
-                        print "Bind " + arg + " already existed, overwriting"
-                else:
-                    binds[arg] = lineNo
-                    output.append(line)
-                    if verbose:
-                        print "New bind " + arg + " added"
-            elif command == "exec":
-                if arg in loaded_files.keys():
-                    if verbose:
-                        print "File " + arg + " already loaded"                      
-                else:
-                    output.append(line)
-                    loaded_files[arg] = lineNo
-                    if verbose:
-                        print "New file " + arg + " added"
-                    if recursive:
-                        # TODO add protection for infinite recursion
-                        # including configs that exec themselves and
-                        # config a execing config b which itself execs config a
-                        # add ./ to prevent files in subfolders being opened in /
-                        try:
-                            print "Recursively cleaning " + arg + ".cfg"                            
-                            rec_file = open("./" + arg + ".cfg")
-                            parseFile(rec_file)
-                            rec_file.close()
-                        except IOError as e:
-                            print "Couldn't open " + arg + ".cfg: {0}".format(e.strerror)
-                            
-            else:
-                output.append(line)
-        except IndexError:
-            print "Invalid line at line " + str(lineNo + 1) + ". Ignoring line." 
-            lineNo -= 1
+                if verbose:
+                    print "Line " + lineNo + " has no argument, copying anyway"
 
     if verbose:
-        print "Writing commands to file:"            
+        print "Writing commands to file:"
     for l in output:
         outfile.write(l + "\n")
         if verbose:
             print "\t" + l
+
+    return lineNo
+
+
+def validate(line):
+    return
+
 
 if __name__ == "__main__":
     Main()
